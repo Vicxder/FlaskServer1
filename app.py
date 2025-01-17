c8ccc1ee7745f7d70e6f85fdd1b971ec8d7eecd0
Vicxder <Vicxder@gmail.com>
2025-01-17 01:48:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 @socketio.on('message')
 def handle_message(message):
